f420419bd36e8cfd0fc2b096219ab404711560e2
abhinaysharma <abhinaysharma127@gmail.com>
2025-10-08 23:27:49 +0530

    fixhome page ui

--- src/components/contents/Content1.jsx
+++ src/components/contents/Content1.jsx
@@ -10,32 +10,32 @@ const Content1 = ({ darkMode }) => {
       className={`pt-16 min-h-screen flex items-center justify-center ${darkMode ? "bg-gray-900" : "bg-yellow-200 text-[#343a40]"
         }`}
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-6xl px-6 md:px-10">
-        <div className="flex flex-col justify-center items-center md:items-start text-center md:text-left space-y-4">
-          <h4 className="text-4xl md:text-[3rem] tracking-tight font-normal">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-6xl px-6 md:px-10 p-4 md:py-0">
+        <div className="flex flex-col justify-center items-center md:items-start text-center md:text-left space-y-3">
+          <h4 className="text-3xl md:text-[3rem] tracking-tight font-normal">
             Hii, I'M A
           </h4>
 
           {/* Typing Effect Name */}
-          <h4 className="text-3xl md:text-[3.5rem] font-bold tracking-tight typing">
+          <h4 className="text-2xl md:text-[3.5rem] font-bold tracking-tight typing">
             ABHINAY SHARMA
           </h4>
 
-          <h3 className="text-2xl md:text-[2rem] tracking-tight font-normal">
+          <h3 className="text-xl md:text-[2rem]  tracking-tight font-normal">
             SOFTWARE DEVELOPER
           </h3>
 
           <div className="flex flex-wrap justify-center md:justify-start gap-4 pt-2">
             <a
               href="#projects"
-              className="bg-gray-700 hover:bg-gray-800 text-white py-3 px-6 border border-gray-600 rounded-lg shadow transition no-underline"
+              className="bg-gray-700 hover:bg-gray-800 text-white text-sm py-2 px-4  md:px-6 md:py-3 border border-gray-600 rounded-lg shadow transition no-underline"
             >
               View My Work
             </a>
             <a
               href={resumePDF}
               download="Abhinay_Sharma_Resume.pdf"
-              className="bg-gray-700 hover:bg-gray-800 text-white py-3 px-6 border border-gray-600 rounded-lg shadow transition no-underline"
+              className="bg-gray-700 hover:bg-gray-800 text-white text-sm py-2 px-4  md:px-6 md:py-3 border border-gray-600 rounded-lg shadow transition no-underline"
             >
               Download CV
             </a>
@@ -46,7 +46,7 @@ const Content1 = ({ darkMode }) => {
           <img
             src={profileImg}
             alt="Abhinay Sharma"
-            className="w-72 h-72 md:w-96 md:h-96 object-cover rounded-full shadow-lg"
+            className="w-70 h-70 md:w-96 md:h-96 object-cover rounded-full shadow-lg"
           />
         </div>
       </div>
